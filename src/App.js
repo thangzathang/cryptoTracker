c4673657f8bf633c9518448b56970016ea86e5b1
thangzathang <thangzathang@gmail.com>
2021-12-18 10:35:00 +1100

    Intial setup and skeleton code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useData from "./useData";
+import { useState } from "react";
+import Coin from "./Coin";
 
 function App() {
+  const [loading, coins, error] = useData();
+
+  console.log("Type of coins is:", typeof coins);
+
+  let result = coins.map(({ foo }) => foo);
+
+  // console.log(coins);
+
+  // const handleChange = (e) => {
+  //   setSearch(e.target.value);
+  // };
+
+  // const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>hi</h1>
+    // <div className="coin-app">
+    //   <div className="coin-search">
+    //     <h1 className="coin-text">Search a currency</h1>
+    //     <form>
+    //       <input className="coin-input" type="text" onChange={handleChange} placeholder="Search" />
+    //     </form>
+    //   </div>
+    //   {filteredCoins.map((coin) => {
+    //     return <Coin key={coin.id} name={coin.name} price={coin.current_price} symbol={coin.symbol} marketcap={coin.total_volume} volume={coin.market_cap} image={coin.image} priceChange={coin.price_change_percentage_24h} />;
+    //   })}
+    // </div>
   );
 }
 
---
+++ src/useData.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useReducer, useState } from "react";
+import axios from "axios";
+
+const APILink = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+export default function useData() {
+  let [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "LOADING": {
+          return { ...state, loading: true };
+        }
+        case "RESOLVED": {
+          return {
+            ...state,
+            loading: false,
+            response: action.response,
+            error: null,
+          };
+        }
+        case "ERROR": {
+          return {
+            ...state,
+            loading: false,
+            response: null,
+            error: action.error,
+          };
+        }
+        default:
+          return state;
+      }
+    },
+    {
+      loading: false,
+      response: null,
+      error: null,
+    }
+  );
+
+  useEffect(() => {
+    let isCurrent = true;
+    dispatch({ type: "LOADING" });
+    axios
+      .get(APILink)
+      .then((res) => {
+        if (isCurrent) {
+          dispatch({ type: "RESOLVED", response: res.data });
+        }
+      })
+      .catch((error) => {
+        dispatch({ type: "ERROR", error });
+      });
+    return () => {
+      isCurrent = false;
+    };
+  }, []);
+
+  return [state.loading, state.response, state.error];
+}
