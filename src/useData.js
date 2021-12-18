907a251517f873b1e7c551d10122f45687a73856
thangzathang <thangzathang@gmail.com>
2021-12-18 16:00:38 +1100

    useData api link bug fix

--- src/useData.js
+++ src/useData.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 import axios from "axios";
 
 const APILink = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false";
