68c60a5222be2c8459bf9d9ba152949d7e0b55a0
thangzathang <thangzathang@gmail.com>
2021-12-19 20:24:23 +1100

    Quick tests

--- src/__test__/Coin.test.js
+++ src/__test__/Coin.test.js
@@ -3,21 +3,18 @@ import { render, screen, cleanup } from "@testing-library/react";
 import Coin from "../Coin";
 import btcData from "./btcData";
 
-// test("Should render Coin Component", () => {
-//   const { name, price, symbol, marketcap, volume, image } = btcData;
-//   //   render(<Coin name={name} price={price} symbol={symbol} marketcap={marketcap} volume={volume} image={image} />);
-//   const coinElement = screen.getAllByTestId("bitcoin");
-//   expect(coinElement).toBeInTheDocument();
-//   // Check Texts on screen
-//   expect(coinElement).toHaveTextContent("USD");
-//   expect(coinElement).toHaveTextContent("Price Change");
+test("Should render Coin Component", () => {
+  const { name, price, symbol, marketcap, volume, image } = btcData;
+  render(<Coin name={name} price={price} symbol={symbol} marketcap={marketcap} volume={volume} image={image} />);
+  const coinElement = screen.getAllByTestId("bitcoin");
+  expect(coinElement).toBeInTheDocument();
+  // Check Texts on screen
+  expect(coinElement).toHaveTextContent("USD");
+  expect(coinElement).toHaveTextContent("Price Change");
 
-//   // Check Texts on screen
-//   expect(coinElement).toHaveClass("coin-price");
-// });
-
-const sum = require("./sum");
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  // Check classnames renders
+  expect(coinElement).toHaveClass("coin-price");
+  expect(coinElement).toHaveClass("coin-data");
+  expect(coinElement).toHaveClass("coin-volume");
+  expect(coinElement).toHaveClass("coin-marketcap");
 });
